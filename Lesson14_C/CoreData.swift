fd99464628b30c7f7b0390d19abe4a446f92cfcd
Данила Дудкин <danildudkin@yandex.ru>
2021-02-17 18:33:05 +0300

    first commit

---
+++ Lesson14_C/CoreData.swift
@@ -0,0 +1,8 @@
+//
+//  CoreData.swift
+//  Lesson14_C
+//
+//  Created by Danila Dudkin on 04.02.2021.
+//
+
+import Foundation
---
+++ Lesson14_C/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  Lesson14_C
+//
+//  Created by Danila Dudkin on 04.02.2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
